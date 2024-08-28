58cc2fc6de784a0c8d85791b91ddfcefc95987be
alexfigueirinha <alex.figueirinha@sp.senai.br>
2024-08-28 15:25:17 -0300

    aula dia 28-08-2024

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,17 +7,58 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    protected $usuarioService; 
+    protected $usuarioService;
 
     public function __construct(UsuarioService $novoUsuarioService)
     {
         $this->usuarioService =  $novoUsuarioService;
     }
 
-    public function store(Request $request){
-        
+    public function store(Request $request)
+    {
+
         $user = $this->usuarioService->create($request->all());
 
-        return $user;         
+        return response()->json($user);
+    }
+
+    public function findById($id)
+    {
+        $result = $this->usuarioService->findById($id);
+
+        return response()->json($result);
+    }
+
+    public function index()
+    {
+        $result = $this->usuarioService->getAll();
+
+        return response()->json($result);
+    }
+
+    public function searchByName(Request $request)
+    {
+        $result = $this->usuarioService->searchByName($request->nome);
+
+        return response()->json($result);
+    }
+
+    public function searchByEmail(Request $request)
+    {
+        $result = $this->usuarioService->searchByEmail($request->email);
+
+        return response()->json($result);
+    }
+
+    public function delete($id)
+    {
+        $result = $this->usuarioService->delete($id);
+
+        return response()->json($result);
+    }
+
+    public function update(Request $request){ 
+        $result = $this->usuarioService->update($request->all());
+        return response()->json($result);
     }
 }
